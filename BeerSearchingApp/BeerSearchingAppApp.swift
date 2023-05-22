614617e6e9ffcb287e745970937b7b8a234af472
Sergio Suarez <ssuarez@baufest.com>
2023-05-22 16:38:28 +0200

    Initial Commit

---
+++ BeerSearchingApp/BeerSearchingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeerSearchingAppApp.swift
+//  BeerSearchingApp
+//
+//  Created by Sergio Suarez on 22/5/23.
+//
+
+import SwiftUI
+
+@main
+struct BeerSearchingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
